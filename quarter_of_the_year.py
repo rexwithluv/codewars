2293f3bfc7acc55fefc4f2b95fba50f05d9e786d
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-05 23:47:10 +0100

    Add files via upload

---
+++ quarter_of_the_year.py
@@ -0,0 +1,18 @@
+""" Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November),
+is part of the fourth quarter.
+
+Constraint:
+
+1 <= month <= 12 """
+
+def quarter_of(month):
+    if month > 9:
+        return 4
+    elif month > 6:
+        return 3
+    elif month > 3:
+        return 2
+    else:
+        return 1
