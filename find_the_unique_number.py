38c14496006be1ba366bd5bba9d6a4a4dda0e9b7
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-05 18:06:35 +0100

    Add files via upload

---
+++ find_the_unique_number.py
@@ -0,0 +1,19 @@
+""" There is an array with some numbers. All numbers are equal except for one. Try to find it!
+
+find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+It’s guaranteed that array contains at least 3 numbers.
+
+The tests contain some very huge arrays, so think about performance.
+
+This is the first kata in series:
+
+Find the unique number (this kata)
+Find the unique string
+Find The Unique
+ """
+
+def find_uniq(arr: list):
+    for counter, i in enumerate(arr):
+        if i != arr[(counter + 1) % len(arr)] and i != arr[counter - 1]:
+            return i
