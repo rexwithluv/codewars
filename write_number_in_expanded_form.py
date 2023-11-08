ca70fa21f02cd44593a20137847176b7b739bb14
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-08 20:25:04 +0100

    Add files via upload

---
+++ write_number_in_expanded_form.py
@@ -0,0 +1,38 @@
+""" Write Number in Expanded Form
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expanded_form(12) # Should return '10 + 2'
+expanded_form(42) # Should return '40 + 2'
+expanded_form(70304) # Should return '70000 + 300 + 4'
+NOTE: All numbers will be whole numbers greater than 0.
+
+If you liked this kata, check out part 2!! """
+
+
+def expanded_form(num: int) -> str:
+    lst = []
+    
+    # Creamos la forma expandida en una lista
+    for position, i in enumerate(str(num)):
+        if i != 0:
+            lst.append(i + ("0" * (len(str(num)) - (position + 1))))
+
+    # Limpiamos la lista expandida por si tuviera algún elemento similar a '0000'
+    clean_lst = []
+    for i in lst:
+        if i[0] != "0":
+            clean_lst.append(i)
+    
+    return " + ".join(clean_lst)   
+
+print(expanded_form(70304))
+
+
+""" def expanded_form(num: int) -> str:
+    chain = ""
+    
+    for position, i in enumerate(str(num)):
+        if i != 0:
+            chain += i + ("0" * (position + 1)) + " + "
+        
+    return chain """
