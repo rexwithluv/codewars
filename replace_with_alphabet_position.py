9a9dcd2940308f56b002dc51749701b0a12ac0e6
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-08 18:40:44 +0100

    Add files via upload

---
+++ replace_with_alphabet_position.py
@@ -0,0 +1,21 @@
+""" Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabet_position("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string ) """
+
+def alphabet_position(text: str) -> str:
+    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers = ""
+
+    for i in text.lower():
+        if i in abc:
+            numbers += str(abc.index(i) + 1) + " "
+
+    return numbers[:len(numbers) - 1]
