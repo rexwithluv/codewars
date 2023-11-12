aa1e6bc3c8585016b1f3a300f537b53ddd94dae9
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-12 16:53:23 +0100

    Add files via upload

---
+++ extract_the_domain_name_from_a_url.py
@@ -0,0 +1,18 @@
+""" Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+* url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+* url = "https://www.cnet.com"                -> domain name = cnet" """
+
+def domain_name(url: str) -> str:
+    url = url.removeprefix("https://")
+    url = url.removeprefix("http://")
+    url = url.removeprefix("www.")
+    
+    if "." in url:
+        url = url[:url.find(".")]
+
+    
+    return url
+    
+print(domain_name("http://github.com"))
