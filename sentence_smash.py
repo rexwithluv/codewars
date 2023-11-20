6a6dbdbb21170b057662e6c298d4a2570c7cd461
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-11-20 20:40:27 +0100

    Add files via upload

---
+++ sentence_smash.py
@@ -0,0 +1,16 @@
+""" Sentence Smash
+Write a function that takes an array of words and smashes them together
+into a sentence and returns the sentence. You can ignore any need to
+sanitize words or add punctuation, but you should add spaces between
+each word. Be careful, there shouldn't be a space at the beginning or
+the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  ->  'hello world this is
+great' """
+
+from typing import List
+
+
+def smash(words: List[str]) -> str:
+    return " ".join(words)
