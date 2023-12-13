daee680de53c92a593d42b4c0498811d288b26fc
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-13 23:10:35 +0100

    Add files via upload

---
+++ the-shell-game.py
@@ -0,0 +1,36 @@
+""" "The Shell Game" involves cups upturned on a playing surface, with a
+ball placed underneath one of them. The index of the cups are swapped
+around multiple times. After that the players will try to find which cup
+contains the ball.
+
+Your task is as follows. Given the cup that the ball starts under, and
+list of swaps, return the location of the ball at the end. Cups are
+given like array/list indices.
+
+For example, given the starting position 0 and the swaps
+[(0, 1), (1, 2), (1, 0)]:  
+
+- The first swap moves the ball from 0 to 1
+- The second swap moves the ball from 1 to 2
+- The final swap doesn't affect the position of the ball.
+
+So
+find_the_ball(0, [(0, 1), (2, 1), (0, 1)]) == 2
+
+There aren't necessarily only three cups in this game, but there will be
+at least two. You can assume all swaps are valid, and involve two
+distinct indices. """
+
+from typing import List, Tuple
+
+
+def find_the_ball(start: int, swap: [List[Tuple[int]]]):
+    for i in swap:
+        if i[0] == start:
+            start = i[1]
+        elif i[1] == start:
+            start = i[0]
+
+    return start
+
+print(find_the_ball(0,[(0, 1), (2, 1), (0, 1)]))
