4408d8d16f6ac7937446c72762fa3c999ff5ade6
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-17 16:15:58 +0100

    Update leap-years.py

--- leap-years.py
+++ leap-years.py
@@ -1,4 +1,5 @@
-""" In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+""" In this kata you should simply determine, whether a given year is a
+leap year or not. In case you don't know the rules, here they are:
 
 * Years divisible by 4 are leap years,
 * but years divisible by 100 are **not** leap years,
