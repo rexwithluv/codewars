a2288108ba811bc5713c148a52c56a1769d3ffc6
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-18 17:56:08 +0100

    Add files via upload

---
+++ returning-strings.py
@@ -0,0 +1,17 @@
+""" ~~~if:sql
+Write a select statement that takes `name` from `person` table and
+return `"Hello, <name> how are you doing today?"` results in a column
+named `greeting`
+~~~
+~~~if-not:sql
+Make a function that will return a greeting statement that uses an
+input; your program should return, `"Hello, <name> how are you doing
+today?"`.
+~~~
+
+*[Make sure you type the exact thing I wrote or the program may not
+execute properly]* """
+
+
+def greet(name: str) -> str:
+    return f"Hello, {name} how are you doing today?"
