17c2e98dd7be43c3c3e13c9e503a49bf10327f00
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-24 00:27:55 +0100

    Add files via upload

---
+++ simple-multiplication.py
@@ -0,0 +1,6 @@
+""" This kata is about multiplying a given number by eight if it is an
+even number and by nine otherwise. """
+
+
+def simple_multiplication(number: int) -> int:
+    return number * 8 if number % 2 == 0 else number * 9
