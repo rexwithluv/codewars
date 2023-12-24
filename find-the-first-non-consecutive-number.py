c1df28b18427fa5445e705ef61ff3eda416d90a5
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2023-12-24 17:19:05 +0100

    Add files via upload

---
+++ find-the-first-non-consecutive-number.py
@@ -0,0 +1,57 @@
+""" Your task is to find the first element of an array that is not
+consecutive.
+
+By not consecutive we mean not exactly 1 larger than the previous
+element of the array.
+
+E.g. If we have an array `[1,2,3,4,6,7,8]` then `1` then `2` then `3`
+then `4` are all consecutive but `6` is not, so that's the first
+non-consecutive number.
+
+```if:c
+If a non consecutive number is found then return `true` and set the
+passed in pointer to the number found.
+
+If the whole array is consecutive then return `false`.
+```
+```if-not:c
+If the whole array is consecutive then return `null`<sup>2</sup>.
+```
+The array will always have at least `2` elements<sup>1</sup> and all
+elements will be numbers. The numbers will also all be unique and in
+ascending order. The numbers could be positive or negative and the first
+non-consecutive could be either too!
+
+If you like this Kata, maybe try this one next:
+https://www.codewars.com/kata/represent-array-of-numbers-as-ranges
+
+```if:c
+<sup>1</sup> Can you write a solution that will return `false` for both
+`[]` and `[ x ]` (an empty array and one with a single number) though?
+(This is an empty array and one with a single number and is not tested
+for, but you can write your own example test. )
+```
+```if-not:c
+<sup>1</sup> Can you write a solution that will return
+`null`<sup>2</sup> for both `[]` and `[ x ]` though?  (This is an empty
+array and one with a single number and is not tested for, but you can
+write your own example test. )
+
+<sup>2</sup>  
+Swift, Ruby and Crystal: `nil`  
+Haskell: `Nothing`  
+Python, Rust, Scala: `None`  
+Julia: `nothing`  
+Nim: `none(int)` (See [options](https://nim-lang.org/docs/options.html))
+``` """
+from typing import List, Union
+
+
+def first_non_consecutive(arr: List[int]) -> Union[int, None]:
+    consecutive = [i for i in range(arr[0], arr[-1] + 1)]
+    if arr == consecutive:
+        return None
+    else:
+        for position, i in enumerate(arr):
+            if i != consecutive[position]:
+                return i
---
+++ find-the-stray-number.py
@@ -0,0 +1,21 @@
+""" You are given an *odd-length* array of integers, in which all of
+them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single
+different number.
+
+**The input array will always be valid!** (odd-length >= 3)
+
+## Examples
+
+```
+[1, 1, 2] ==> 2
+[17, 17, 3, 17, 17, 17, 17] ==> 3
+``` """
+
+from typing import List
+
+
+def stray(arr: List[int]) -> int:
+    arr.sort()
+    return arr[-1] if arr[0] == arr[1] else arr[0]
