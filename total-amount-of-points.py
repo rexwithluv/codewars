f27b8d761285cba2409d6e135eaea5b87785024b
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2024-01-01 13:14:36 +0100

    Add files via upload

---
+++ total-amount-of-points.py
@@ -0,0 +1,34 @@
+""" Our football team has finished the championship.
+
+Our team's match results are recorded in a collection of strings. Each
+match is represented by a string in the format `"x:y"`, where `x` is our
+team's score and `y` is our opponents score.
+
+For example:
+```["3:1", "2:2", "0:1", ...]```
+
+Points are awarded for each match as follows:
+- if x > y: 3 points (win)
+- if x < y: 0 points (loss)
+- if x = y: 1 point  (tie)
+
+We need to write a function that takes this collection and returns the
+number of points our team (`x`) got in the championship by the rules
+given above.
+
+Notes:
+- our team always plays 10 matches in the championship
+- 0 <= x <= 4
+- 0 <= y <= 4 """
+
+
+def points(games):
+    counter = 0
+    for i in games:
+        i = i.split(":")
+        if i[0] > i[1]:
+            counter += 3
+        elif i[1] == i[0]:
+            counter += 1
+
+    return counter
