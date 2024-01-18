0652efa7a5d63fa4f21f8e3644027836265cbf01
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2024-01-18 22:10:06 +0100

    im here again <3

---
+++ a-letters-best-friend.py
@@ -0,0 +1,37 @@
+""" ### Task
+Given a string, return if a given letter always appears immediately
+before another given letter.
+
+### Worked Example
+```
+("he headed to the store", "h", "e") ➞ True
+
+# All occurences of "h": ["he", "headed", "the"]
+# All occurences of "h" have an "e" after it.
+# Return True
+
+('abcdee', 'e', 'e') ➞ False
+
+# For first "e" we can get "ee"
+# For second "e" we cannot have "ee"
+# Return False
+```
+### Examples
+```
+("i found an ounce with my hound", "o", "u") ➞ True
+
+("we found your dynamite", "d", "y") ➞ False
+```
+### Notes
+- All sentences will be given in lowercase. """
+
+
+def best_friend(txt, a, b):
+    try:
+        for i in range(len(txt)):
+            if txt[i] == a and txt[i + 1] != b:
+                return False
+
+        return True
+    except IndexError:
+        return False
