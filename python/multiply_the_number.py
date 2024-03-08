952061fb62148bfcc1784b784e1fc23521921153
RexWithLuv <aaronaax@gmail.com>
2024-03-08 20:26:06 +0100

    Kata

---
+++ python/multiply_the_number.py
@@ -0,0 +1,17 @@
+"""ID: 5708f682c69b48047b000e07
+
+Jack really likes his number five: the trick here is that you have to
+multiply each number by 5 raised to the number of digits of each
+numbers, so, for example:
+
+```
+  3 -->    15  (  3 * 5¹)
+ 10 -->   250  ( 10 * 5²)
+200 --> 25000  (200 * 5³)
+  0 -->     0  (  0 * 5¹)
+ -3 -->   -15  ( -3 * 5¹)
+```"""
+
+
+def multiply(n: int) -> int:
+    return n * 5 ** len(str(abs(n)))
