e8d33f28955fb20ea3a703f073e7e5e8b73e21a8
RexWithLuv <aaronaax@gmail.com>
2024-03-16 17:17:11 +0100

    Added 2

---
+++ python/build_a_square.py
@@ -0,0 +1,20 @@
+"""ID: 59a96d71dbe3b06c0200009c
+
+I will give you an integer. Give me back a shape that is as long and
+wide as the integer. The integer will be a whole number between 1 and
+50.
+
+
+## Example
+
+`n = 3`, so I expect a 3x3 square back just like below as a string:
+
+```
++++
++++
++++
+```"""
+
+
+def generate_shape(n):
+    return "\n".join("".join("+" for i in range(n)) for j in range(n))
---
+++ python/evil_or_odious.py
@@ -0,0 +1,22 @@
+"""ID: 56fcfad9c7e1fa2472000034
+
+The number n is <b>Evil</b> if it has an even number of 1's in its
+binary representation.</br>
+The first few Evil numbers: 3, 5, 6, 9, 10, 12, 15, 17, 18, 20</br></br>
+The number n is <b>Odious</b> if it has an odd number of 1's in its
+binary representation.</br>
+The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16,
+19</br></br>
+You have to write a function that determine if a number is Evil of
+Odious, function should return "It's Evil!" in case of evil number and
+"It's Odious!" in case of odious number.
+
+good luck :)"""
+
+
+def evil(n):
+    return "It's Odious!" if str(bin(n)).count("1") % 2 == 1 else "It's Evil!"
+
+
+print(1)
+print(bin(1))
