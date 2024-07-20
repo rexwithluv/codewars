81aa036c617e9028dca01cab9a6e9c6b39a47844
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2024-07-20 16:29:06 +0200

    Daily kata

---
+++ python/return_two_highest_values_in_list.py
@@ -0,0 +1,17 @@
+"""ID: 57ab3c09bb994429df000a4a
+
+In this kata, your job is to return the two distinct highest values in a list. If there're less than 2 unique values, return as many of them, as possible.
+
+The result should also be ordered from highest to lowest.
+
+Examples:
+
+```
+[4, 10, 10, 9]  =>  [10, 9]
+[1, 1, 1]  =>  [1]
+[]  =>  []
+```"""
+
+
+def two_highest(numbers: list[int]) -> list[int | None]:
+    return sorted(list(set(numbers)), reverse=True)[:2]
