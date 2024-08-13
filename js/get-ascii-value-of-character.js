05d4054e1e4138ed2fab3d67dade5df58a1caed9
rexwithluv <aaronaax@gmail.com>
2024-08-13 12:21:48 +0200

    daily kata (js and python)

---
+++ js/get-ascii-value-of-character.js
@@ -0,0 +1,9 @@
+/* ID: 55acfc59c3c23d230f00006d
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/ */
+
+function getASCII(c) {
+  return c.charCodeAt(0);
+}
---
+++ python/get_ascii_value_of_character.py
@@ -0,0 +1,9 @@
+"""ID: 55acfc59c3c23d230f00006d
+
+Get ASCII value of a character.
+
+For the ASCII table you can refer to http://www.asciitable.com/"""
+
+
+def get_ascii(ch: str) -> int:
+    return ord(ch)
