5f887d6d15c84dcaa50b50b1e84822641561542b
rexwithluv <aaronaax@gmail.com>
2024-08-18 21:38:45 +0200

    daily kata

---
+++ js/basic-training-add-item-to-an-array.js
@@ -0,0 +1,24 @@
+/* ID: 511f0fe64ae8683297000001
+
+Add the value "codewars" to the websites array.
+After your code executes the websites array **should == ["codewars"]**
+
+The websites array has **already been defined for you** using the following code:
+
+```python
+websites = []
+```
+```javascript
+var websites = [];
+```
+```coffeescript
+websites = []
+```
+```ruby
+$websites = []
+```
+```typescript
+export const websites: string[] = [];
+``` */
+
+websites.push("codewars");
