1122633bb73c8d125ad0daa1aea3264338bcb9b3
rexwithluv <aaronaax@gmail.com>
2024-09-20 18:42:45 +0200

    daily kata

---
+++ python/indexed_capitalization.py
@@ -0,0 +1,30 @@
+"""ID: 59cfc09a86a6fdf6df0000f1
+
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+For example:
+
+* `capitalize("abcdef",[1,2,5]) = "aBCdeF"`
+* `capitalize("abcdef",[1,2,5,100]) = "aBCdeF"`. There is no index 100.
+
+The input will be a lowercase string with no spaces and an array of digits.
+
+Good luck!
+
+Be sure to also try:
+
+[Alternate capitalization](https://www.codewars.com/kata/59cfc000aeb2844d16000075)
+
+[String array
+revisal](https://www.codewars.com/kata/59f08f89a5e129c543000069)"""
+
+
+def capitalize(s: str, ind: list[int]) -> str:
+    s = list(s)
+    for i in ind:
+        try:
+            s[i] = s[i].upper()
+        except IndexError:
+            break
+
+    return "".join(s)
