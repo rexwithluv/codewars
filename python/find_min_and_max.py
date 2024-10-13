0f0be077f6126d06bb5352aaf5b5ff3ede04b2b7
rexwithluv <aaronaax@gmail.com>
2024-10-13 10:46:00 +0200

    daily kata

---
+++ python/find_min_and_max.py
@@ -0,0 +1,8 @@
+"""ID: 57a1ae8c7cb1f31e4e000130
+
+Implement a function that returns the minimal and the maximal value of a
+list (in this order)."""
+
+
+def get_min_max(seq):
+    return min(seq), max(seq)
