b991bf81d37b7b1b967e72bb47048108a63a1a26
rexwithluv <aaronaax@gmail.com>
2024-10-24 21:26:17 +0200

    daily kata

---
+++ js/substituting-variables-into-strings-padded-numbers.js
@@ -0,0 +1,26 @@
+/* ID: 51c89385ee245d7ddf000001
+
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE"  where value is a 5 digit padded number.
+
+Example:
+
+```cpp
+solution(5); // should return "Value is 00005"
+```
+```c
+solution(5); // should return "Value is 00005"
+```
+```factor
+5 solution ! should return "Value is 00005"
+```
+```haskell
+solution 5  -- should return "Value is 00005"
+```
+```javascript
+solution(5) // should return "Value is 00005"
+```
+```python
+solution(5)  # should return "Value is 00005"
+``` */
+
+const solution = (value) => "Value is " + value.toString().padStart(5, "0");
