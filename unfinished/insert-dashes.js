a960c313cc61c0ae13a36de973c4b93259f5722d
rexwithluv <aaronaax@gmail.com>
2024-11-24 13:15:59 +0100

    daily kata 3

--- unfinished/insert-dashes.js
+++ unfinished/insert-dashes.js
@@ -11,3 +11,21 @@ Write a function that takes an integer `num` (`num >= 0`) and inserts dashes (`'
 13579  ---> "1-3-5-7-9"
  86420 ---> "86420"
 ``` */
+
+"use strict";
+
+
+const insertDash = (numbers) => {
+     const isOdd = num => num % 2 == 1;
+
+     let newString = "0";
+     Array.from(numbers.toString()).forEach(num => {
+          if (isOdd(parseInt(num)) && isOdd(parseInt(newString.charAt(newString.length - 1)))) {
+               newString += "-" + num
+          } else {
+               newString += num
+          }
+     });
+
+     return newString.slice(1);
+}
