60dd72529368065aecf6ff6df8bb1c4418113522
rexwithluv <aaronaax@gmail.com>
2024-11-29 23:07:25 +0100

    daily kata

---
+++ js/enumerable-magic-does-my-list-include-this.js
@@ -0,0 +1,11 @@
+/* ID: 545991b4cbae2a5fda000158
+
+Create a method that accepts a list and an item, and returns `true` if the item belongs to the list, otherwise `false`.
+
+~~~if:ruby
+If you need help, here's some reference material: http://www.rubycuts.com/enum-include
+~~~ */
+
+function include(arr, item) {
+    return arr.includes(item);
+}
