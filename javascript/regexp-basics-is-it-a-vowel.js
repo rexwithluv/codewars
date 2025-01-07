a3e180d031db86655d529c381aa230f9180c1f30
rexwithluv <aaronaax@gmail.com>
2025-01-07 16:42:28 +0100

    daily kata

---
+++ javascript/regexp-basics-is-it-a-vowel.js
@@ -0,0 +1,8 @@
+/* URL: https://www.codewars.com/kata/567bed99ee3451292c000025
+
+Implement the function which should return `true` if given object is a vowel (meaning `a, e, i, o, u`, uppercase or lowercase), and `false` otherwise.
+*/
+
+String.prototype.vowel = function () {
+  return /^[aeiou]$/i.test(this);
+};
