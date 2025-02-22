a33ac92059ff4258577424a71e6f0fb71ec05364
rexwithluv <aaronaax@gmail.com>
2025-02-22 21:38:43 +0100

    daily kata

---
+++ javascript/sort-arrays-1.js
@@ -0,0 +1,19 @@
+/* ID: 51f41b98e8f176e70d0002a8
+
+Just a simple sorting usage. Create a function that returns the elements of the
+input-array / list sorted in lexicographical order.*/
+
+"use strict";
+
+// input: names - unsorted array
+// output: sorted array
+
+/**
+ *
+ * @param     {Array[string]}   names   Array of strings
+ * @returns   {Array[string]}           Array of sorted by lexical order
+ */
+
+function sortme(names) {
+  return names.sort();
+}
