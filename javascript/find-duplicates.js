6d664bf83f3955315e8f5b5b759c4eacf3358e5c
rexwithluv <aaronaax@gmail.com>
2025-02-25 21:56:51 +0100

    daily kata

---
+++ javascript/find-duplicates.js
@@ -0,0 +1,23 @@
+/* URL: https://www.codewars.com/kata/5558cc216a7a231ac9000022
+
+Given an array, find the duplicates in that array, and return a new array of those duplicates. The elements of the returned array should appear in the order when they first appeared as duplicates.
+
+__*Note*__: numbers and their corresponding string representations should not be treated as duplicates (i.e., `"1" != 1`).
+
+## Examples
+```
+[1, 2, 4, 4, 3, 3, 1, 5, 3, "5"]  ==>  [4, 3, 1]
+[0, 1, 2, 3, 4, 5]                ==>  []
+```
+*/
+
+"use strict";
+
+function duplicates(arr) {
+    let map = new Map();
+    let set = new Set();
+    arr.forEach(e => {
+        map.get(e) ? set.add(e) : map.set(e, true)
+    })
+    return Array.from(set)
+}
